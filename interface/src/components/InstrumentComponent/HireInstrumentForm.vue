16ae5f8cdb057efcd630d40473ff6fc0a04ed63b
LEE KA LONG <Erik@LEEs-MBP.home>
2018-05-21 00:10:05 +1000

    Hire Instrument Page UI Finished

---
+++ interface/src/components/InstrumentComponent/HireInstrumentForm.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="columns">
+        <div class="column is-1"></div>
+        <div class="column is-10">
+            <h1 class="has-text-centered title is-1">Hire Instrument</h1>
+                <!--  Student ID and Student Name field -->
+                <div class="field is-horizontal"> 
+                    <div class="field-label is-normal">
+                        <label class="label">Student ID</label>
+                    </div>
+
+                    <div class="field-body">
+                        <div class="field">
+                            <p class="control is-expanded">
+                                <input class="input" type="text" placeholder="Student ID">
+                            </p>
+                        </div>
+
+                    <div class="field-label is-normal">
+                        <label class="label">Student Name</label>
+                    </div>
+                        <div class="field">
+                            <p class="control is-expanded">
+                                <input class="input" type="text" placeholder="e.g John Snow" value="">
+                            </p>
+                        </div>
+                    </div>
+                </div> 
+                <!-- End of Student ID and Student Name field -->
+
+                <!--  Date field -->
+                <div class="field is-horizontal"> 
+                    <div class="field-label is-normal">
+                        <label class="label">From</label>
+                    </div>
+
+                    <div class="field-body">
+                        <div class="field">
+                            <p class="control is-expanded">
+                                <input class="input" type="date" min="01-01-2018" max="31-12-2018">
+                            </p>
+                        </div>
+
+                    <div class="field-label is-normal">
+                        <label class="label">To ... </label>
+                    </div>
+                        <div class="field">
+                            <p class="control is-expanded">
+                                <input class="input" type="date" min="01-01-2018" max="31-12-2018">
+                            </p>
+                        </div>
+                    </div>
+                </div> 
+                <!-- End of Date field -->
+            
+            <div class="has-text-centered">
+                <button class="button is-success">Send Request</button>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+<style scoped>
+
+</style>
--- interface/src/components/pages/HireInstrument.vue
+++ interface/src/components/pages/HireInstrument.vue
@@ -1,9 +1,26 @@
 <template>
     <div>
       <Navbar class="nav"/>
-        <div class="main-content">
-            <InstrumentCard/>
-        </div>
+            <!-- Instrument Information Banner -->
+            <section class="hero card is-small">
+                <div class="hero-body">
+                    <div class="container">
+                        <div class="columns">
+                            <div class="column is-4">
+                                <img class="image is-Square" src="https://d1aeri3ty3izns.cloudfront.net/media/19/194914/1200/preview.jpg">
+                            </div>
+                            <div class="has-text-centered column is-8">
+                                <h1 class="title is-1">Instrument Name : </h1>
+                                <h1 class="title is-2">Condition : </h1>
+                                <h1 class="title is-2">Cost : </h1>
+                                <h1 class="title is-2">In Stock : </h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section> <!-- Hero Card End -->
+
+            <HireInstrumentForm/>
       <Footer/>
     </div>
 </template>
@@ -11,16 +28,20 @@
 
 import Footer from '../Footer'
 import Navbar from '../Navbar'
-import InstrumentCard from '../InstrumentComponent/InstrumentCard'
+import HireInstrumentForm from '../InstrumentComponent/HireInstrumentForm'
+
 export default {
     components:{
         Footer,
         Navbar,
-        InstrumentCard
+        HireInstrumentForm
     }
   
 }
 </script>
 <style scoped>
+.card{
+    background-color: aliceblue;
+}
 
 </style>
