81c26a6a1f211efbaa1d31eb767cf29da74be918
LEE KA LONG <Erik@LEEs-MacBook-Pro.local>
2018-05-25 00:36:20 +1000

    deployment testing 2

---
+++ website/ip.js
@@ -0,0 +1,4 @@
+var ip = require('ip');
+// Front request to Server
+
+console.log(address)
--- website/src/services/Api.js
+++ website/src/services/Api.js
@@ -1,11 +1,14 @@
 const axios = require('axios')
 var ip = require('ip');
 // Front request to Server
-const address = ip.address()
+const address = req.connection.localAddress
+
+console.log(address)
 
 export default () => {
   // create axios object point to our backend URL 8081
   return axios.create({
-    baseURL: `http://${address}:8081/` // Connect to our backend API,
+    baseURL: `http://18.216.247.248:8081/` // Connect to our backend API,
+
   })
 }
