82f4a7e4dd06bae9fe240fe4d6471fe79eeb864c
LEE KA LONG <Erik@LEEs-MacBook-Pro.local>
2018-05-25 00:39:59 +1000

    dist file testing

--- website/src/services/Api.js
+++ website/src/services/Api.js
@@ -1,14 +1,9 @@
 const axios = require('axios')
-var ip = require('ip');
 // Front request to Server
-const address = req.connection.localAddress
-
-console.log(address)
 
 export default () => {
   // create axios object point to our backend URL 8081
   return axios.create({
     baseURL: `http://18.216.247.248:8081/` // Connect to our backend API,
-
   })
 }
