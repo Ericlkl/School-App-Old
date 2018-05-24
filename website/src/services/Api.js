2669bfd3a80544d4f244632ad3816070fee140e0
LEE KA LONG <Erik@LEEs-MacBook-Pro.local>
2018-05-24 22:46:27 +1000

    Added IP module, Deploy Seuccessfully on AMS but can not get DB data

--- website/src/services/Api.js
+++ website/src/services/Api.js
@@ -1,9 +1,11 @@
 const axios = require('axios')
+var ip = require('ip');
 // Front request to Server
+const address = ip.address()
 
 export default () => {
   // create axios object point to our backend URL 8081
   return axios.create({
-    baseURL: `http://localhost:8081/` // Connect to our backend API,
+    baseURL: `http://${address}:8081/` // Connect to our backend API,
   })
 }
