7ba7d3b1aeb5cb085b1ef9c2f0f0eee96337bc7b
LEE KA LONG <Erik@LEEs-MacBook-Pro.local>
2018-05-28 23:00:44 +1000

    Form Validation move to checkValidation Folder

---
+++ website/checkValidation/ValidationController.js
@@ -0,0 +1,113 @@
+// Regular Express for Email format
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+// Regular Expression for only accept numbers
+const numbersOnlyRegex = /^[0-9]*$/
+// Regular Expression for only accept English letter and space
+const letter_space_Regex = /^[a-zA-Z_ ]*$/
+
+const letters_only_Regex = /^[a-zA-Z]*$/
+
+
+
+export default {
+
+    checkContactUsForm(dataPackage){
+
+        /*
+            Initialise the alertMsg variable, it is used to pass the text string to alret the user
+            their information in incorrect
+        */
+        //  Testing for Message Box
+
+        var alertMsg = (dataPackage.message.length === 0) ? "Please insert Your Message!" : null
+
+
+        // Testing for phone number input field, only accept number and must be 9 digit long
+        alertMsg = (!numbersOnlyRegex.test(dataPackage.phonenumber)) ? "Invalid phone number detected! Please correct it!" : alertMsg
+        alertMsg = (dataPackage.phonenumber.length != 9) ? "There must be 9 digit for the phone number!" : alertMsg
+        alertMsg = (dataPackage.phonenumber.length === 0) ? "Please Insert Your Phone Number !" : alertMsg
+
+        // Testing for email input field, using Regular expression to check the email format
+        alertMsg = (!emailRegex.test(dataPackage.email)) ? "Invalid emaill format detected! Please Correct it!" : alertMsg
+        alertMsg = (dataPackage.email.length === 0) ? "Please insert Your Email Address!" : alertMsg
+
+        // Testing for name input field, only accept english letter a-z, A-Z and space _ 
+        alertMsg = (!letter_space_Regex.test(dataPackage.fullname)) ? "Invaild name detected! Only accept english letter!" : alertMsg
+        alertMsg = (dataPackage.fullname.length === 0) ? "Please insert Your Name!" : alertMsg
+
+        return alertMsg
+
+    },
+    checkHireInstrumentForm(dataPackage){
+
+          // Create Date Object for comparing start and end date
+          const start = new Date(dataPackage.startDate)
+          const end = new Date(dataPackage.endDate)
+
+            /*
+                Initialise the alertMsg variable, it is used to pass the text string to alret the user
+                their information in incorrect
+            */
+
+          // Testing for start date and end date
+          // only accept start date < end date
+          var alertMsg = (start.getTime() === end.getTime()) ? "Start Data and End date Cannot be same !" : null
+          alertMsg = (start > end) ? "The Start Date cannot earlier than end date! " : alertMsg
+          alertMsg = (dataPackage.endDate === null) ? "Please Select the end date !" : alertMsg
+          alertMsg = (dataPackage.startDate === null) ? "Please Select the start date !" : alertMsg
+
+          // Testing for student name input field, only accept english letter a-z, A-Z and space _ 
+          alertMsg = (!letter_space_Regex.test(dataPackage.studentName)) ? "Invaild name detected! Only accept english letter!" : alertMsg
+          alertMsg = (dataPackage.studentName.length === 0) ? "Please insert Your Name!" : alertMsg
+
+          // Testing for student id input field, only accept numbers
+          alertMsg = (!numbersOnlyRegex.test(dataPackage.studentID)) ? "Student Number is inValid! It onlt accept numbers!" : alertMsg
+          alertMsg = (dataPackage.studentID.length === 0) ? "Please Insert Your Student Number !" : alertMsg
+
+          return alertMsg
+
+    },
+    checkEnrolForm(dataPackage) {
+
+      /*
+          Initialise the alertMsg variable, it is used to pass the text string to alret the user
+          their information in incorrect
+      */
+      // Check Terms and Condition box is ticked
+      var alertMsg = (dataPackage.termsChecked === false) ? "Please checked the terms of Condition check box" : null
+
+      // Check Parent Phone Number is valid
+      alertMsg = (!numbersOnlyRegex.test(dataPackage.parentPhoneNumber)) ? "parent phone number is invalid! only accept numbers!" : alertMsg
+
+      alertMsg = (dataPackage.parentPhoneNumber.length != 9) ? "There must be 9 digit for the parent phone number!" : alertMsg
+      alertMsg = (dataPackage.parentPhoneNumber.length === 0) ? "Please Insert Your parent phone Number !" : alertMsg
+
+      // Check parent Name is Valid
+      alertMsg = (!letter_space_Regex.test(dataPackage.parentName)) ? "Invaild parent name detected! Only accept english letter!" : alertMsg
+
+      alertMsg = (dataPackage.parentName.length === 0) ? "Please insert Your Parent full Name!" : alertMsg
+
+      // Check student phone number is Valid
+      alertMsg = (!numbersOnlyRegex.test(dataPackage.phoneNumber)) ? "phone number is invalid! only accept numbers!" : alertMsg
+      alertMsg = (dataPackage.phoneNumber.length != 9) ? "There must be 9 digit for the phone number!" : alertMsg
+      alertMsg = (dataPackage.phoneNumber.length === 0) ? "Please Insert Your Phone Number !" : alertMsg
+
+      // Check student email is Valid
+      alertMsg = (!emailRegex.test(dataPackage.email)) ? "Invalid emaill format detected! Please Correct it!" : alertMsg
+      alertMsg = (dataPackage.email.length === 0) ? "Please insert Your Email Address!" : alertMsg
+
+      // Check student email is Valid
+      alertMsg = (dataPackage.gender === '') ? "Please Select your gender!" : alertMsg
+      alertMsg = (dataPackage.address.length === 0) ? "Please insert Your Address!" : alertMsg
+
+      // Check student firstname is Valid
+      alertMsg = (!letters_only_Regex.test(dataPackage.firstName)) ? "Given name is invalid! Only accept english letter!" : alertMsg
+      alertMsg = (dataPackage.firstName.length === 0) ? "Please insert Your Given Name!" : alertMsg
+
+      // Check student lastname is Valid
+      alertMsg = (!letters_only_Regex.test(dataPackage.lastName)) ? "Family name is invalid! Only accept english letter!" : alertMsg
+      alertMsg = (dataPackage.lastName.length === 0) ? "Please insert Your Family Name!" : alertMsg
+
+      return alertMsg
+    },
+}
--- website/src/components/ContactForm.vue
+++ website/src/components/ContactForm.vue
@@ -7,7 +7,7 @@
         <div class="field">
         <label class="label">Full Name*</label>
         <div class="control">
-            <input class="input" type="text" placeholder="E.g Burce Wayne" v-model="fullname">
+            <input class="input" type="text" placeholder="E.g Burce Wayne" v-model="dataPackage.fullname">
         </div>
         </div>
 
@@ -15,7 +15,7 @@
         <div class="field">
         <label class="label">Email*</label>
         <div class="control">
-            <input class="input" type="text" placeholder="Abc@email.com" v-model="email">
+            <input class="input" type="text" placeholder="Abc@email.com" v-model="dataPackage.email">
         </div>
         </div>
 
@@ -23,7 +23,7 @@
         <div class="field">
         <label class="label">Phone Number*</label>
         <div class="control has-icons-left">
-            <input class="input" type="text" v-model="phonenumber">
+            <input class="input" type="text" v-model="dataPackage.phonenumber">
             <span class="icon is-small is-left">
             <label class="label">+61</label>
             </span>
@@ -32,7 +32,7 @@
 
         <div class="field">
         <div class="control">
-            <textarea class="textarea is-success" type="text" v-model="message" placeholder="Enter your message here"></textarea>
+            <textarea class="textarea is-success" type="text" v-model="dataPackage.message" placeholder="Enter your message here"></textarea>
         </div>
         </div>
 
@@ -43,24 +43,26 @@
 
 <script>
 import Connection from '../services/Connection'
+import ValidationController from '../../checkValidation/ValidationController'
 
     export default {
         data() {
             return {
-                fullname: '',
-                phonenumber: '',
-                email: '',
-                message: '',
-                isValid: false
+                dataPackage: {
+                    fullname: '',
+                    phonenumber: '',
+                    email: '',
+                    message: '',
+                }
             }
         },
         methods : {
             resetInputField(){
-                this.fullname = ''
-                this.phonenumber =  ''
-                this.email =  ''
-                this.message =  ''
-                this.isValid = false
+                this.dataPackage.fullname = ''
+                this.dataPackage.phonenumber =  ''
+                this.dataPackage.email =  ''
+                this.dataPackage.message =  ''
+                this.dataPackage.isValid = false
             },
             showAlertMsg(msg){
                 this.$toast.open({
@@ -68,52 +70,23 @@ import Connection from '../services/Connection'
                     type: 'is-danger'
                 })
             },
-            checkValid(){
-                // Regular Express for Email format
-                const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-                /*
-                    Initialise the alertMsg variable, it is used to pass the text string to alret the user
-                    their information in incorrect
-                */
+            async uploadQuestion() {
 
-                //  Testing for Message Box
-                var alertMsg = (this.message.length === 0) ? "Please insert Your Message!" : null
-
-                // Testing for phone number input field, only accept number and must be 9 digit long
-                alertMsg = (!/^[0-9]*$/.test(this.phonenumber)) ? "Invalid phone number detected! Please correct it!" : alertMsg
-                alertMsg = (this.phonenumber.length != 9) ? "There must be 9 digit for the phone number!" : alertMsg
-                alertMsg = (this.phonenumber.length === 0) ? "Please Insert Your Phone Number !" : alertMsg
-
-                // Testing for email input field, using Regular expression to check the email format
-                alertMsg = (!emailRegex.test(this.email)) ? "Invalid emaill format detected! Please Correct it!" : alertMsg
-                alertMsg = (this.email.length === 0) ? "Please insert Your Email Address!" : alertMsg
-
-                // Testing for name input field, only accept english letter a-z, A-Z and space _ 
-                alertMsg = (!/^[a-zA-Z_ ]*$/.test(this.fullname)) ? "Invaild name detected! Only accept english letter!" : alertMsg
-                alertMsg = (this.fullname.length === 0) ? "Please insert Your Name!" : alertMsg
+                const alertMsg = ValidationController.checkContactUsForm(this.dataPackage)
 
                 if (alertMsg != null){
                     this.showAlertMsg(alertMsg)
-                    return false
+                    return 
                 }
-                // return true for isValid attribute
-                return true
-
-            },
-
-            async uploadQuestion() {
-                this.isValid = this.checkValid()
-                // If the information is not in valid format , stop the function 
-                if (!this.isValid){ return }
 
                 try {
                     // Pass information to port 8081, then update database
                     const response = await Connection.submitQuestion({
-                        FullName : this.fullname,
-                        Email: this.email,
-                        PhoneNumber: parseInt(this.phonenumber),
-                        Message: this.message
+                        FullName : this.dataPackage.fullname,
+                        Email: this.dataPackage.email,
+                        PhoneNumber: parseInt(this.dataPackage.phonenumber),
+                        Message: this.dataPackage.message
                     })
                     // show information box if it is success!
                     this.$toast.open({
